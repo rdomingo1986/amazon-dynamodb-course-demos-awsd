0cb996038dd270b66d7d3772ab3a847c100d951c
Domingo Ramirez <domingo.ramirez.artigas@gmail.com>
2021-06-10 06:10:34 -0500

    change

--- commands.sh
+++ commands.sh
@@ -3,3 +3,7 @@ docker run -p 8000:8000 amazon/dynamodb-local
 o
 
 docker run -p 4566:4566 -p 4571:4571 localstack/localstack
+
+aws dynamodb create-table --table-name localTable --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --billing-mode PAY_PER_REQUEST --endpoint=http://localhost:8000
+
+aws dynamodb list-tables --endpoint=http://localhost:8000 
